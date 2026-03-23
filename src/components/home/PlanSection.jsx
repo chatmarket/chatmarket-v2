efbd07b5b25618937810cea97b886f07fa85a94e
株式会社ONE STEP <unei@chatmarket.info>
2026-03-23 23:31:40 +0000

    File changes

--- src/components/home/PlanSection.jsx
+++ src/components/home/PlanSection.jsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Check, Video, Radio, PhoneCall, Play } from "lucide-react";
+import { base44 } from "@/api/base44Client";
 
 const plans = [
   {
