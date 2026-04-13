50a765d3d739f2b079d5fbe67252044d29f0b8a2
株式会社ONE STEP <unei@chatmarket.info>
2026-04-13 14:09:39 +0000

    File changes

--- src/pages/PlanSelect.jsx
+++ src/pages/PlanSelect.jsx
@@ -299,7 +299,7 @@ export default function PlanSelect() {
 
       {/* プラン一覧 */}
       <div className="space-y-3">
-        <Accordion type="multiple" defaultValue={["basic", "vod", "ppv"]} className="space-y-2">
+        <Accordion type="multiple" defaultValue={["free", "basic", "call-anser", "vod"]} className="space-y-2">
           {PLANS.map((plan) => {
             const Icon = plan.icon;
             const isSelected = selected.has(plan.id);
